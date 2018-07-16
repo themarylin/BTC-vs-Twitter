1932ecf5fd725e45b9043089c4da2fc4614d4b57
Mary Lin <themarylin90@gmail.com>
2018-07-16 12:46:53 -0700

    finished collecting all data

--- Source_codes/target_users.py
+++ Source_codes/target_users.py
@@ -1,4 +1,5 @@
-target_users = [
-    '@6BillionPeople','@officialmcafee','@VitalikButerin','@SatoshiLite','@pmarca','@rogerkver','@aantonop','@nickszabo4','@erikvoorhees','@brian_armstrong'
-    '@CryptoYoda1338','@crypto_rand','@WhalePanda','@peterbrandt','VentureCoinist','@cz_binance','@tuurdemeester'
-    '@lopp','@barryslilbert','@VinnyLingham']
+target_users = ['@6BillionPeople','@VitalikButerin','@SatoshiLite','@pmarca','@rogerkver','@aantonop','@nickszabo4','@erikvoorhees',
+    '@brian_armstrong','@CryptoYoda1338','@crypto_rand','@WhalePanda','@peterbrandt','@VentureCoinist','@cz_binance','@tuurdemeester'
+    '@lopp','@barryslilbert','@VinnyLingham', '@brian_armstrong', '@rogerkver', '@tylerwinklevoss', '@NickSzabo4', '@dtapscott', 
+    '@jihanwu', '@laurashin', '@thomaspower', '@barrysilbert', '@TuurDemeester'
+]
